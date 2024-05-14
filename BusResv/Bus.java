59cbac718ef114ae6be2641cc8fd0a5d9bb7c147
Vikram-MS <vikrammuthuvel2002@gmail.com>
2024-05-14 22:40:15 +0530

    Add files via upload

---
+++ BusResv/Bus.java
@@ -0,0 +1,34 @@
+package BusResv;
+
+public class Bus {
+	private int busNo;
+	private boolean ac;
+	private int capacity;
+	
+	Bus(int no,boolean ac,int cap){
+		this.busNo=no;
+		this.ac=ac;
+		this.capacity=cap;
+	}
+	
+	public int getbusNo(){
+		return busNo;
+	}
+	
+	public boolean isAc(){
+		return ac;
+	}
+	
+	public void setAc(boolean val) {
+		ac=val;
+	}
+	
+	public int getCapacity(){
+		return capacity;
+	}
+	
+	public void setCapacity(int cap) {
+		capacity=cap;
+	}
+	
+}
